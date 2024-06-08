c5a7cd7d61e4fa8508f52f2208dd3fe4ba9c760b
Ruiio <103175389+Ruiio@users.noreply.github.com>
2024-06-08 17:02:28 +0800

    Update edu.py

--- streamlit/edu.py
+++ streamlit/edu.py
@@ -2,8 +2,11 @@
 import matplotlib
 import matplotlib.pyplot as plt
 def getEduPic(df):
-    font = {'family': 'Microsoft YaHei', 'weight': 'bold'}
-    matplotlib.rc("font", **font)
+    matplotlib.font_manager.fontManager.addfont('simfang.ttf')
+
+    plt.rcParams['font.sans-serif']=['simfang'] #用来正常显示中文标签
+
+    plt.rcParams['axes.unicode_minus']=False#用来正常显示负号
 
     # 确保文件路径正确
     #file_path = r'F:\dataAny\\alldata.csv'
@@ -43,8 +46,11 @@ def getEduPic(df):
 
 
 def plot_gender_distribution(df):
-    font = {'family': 'Microsoft YaHei', 'weight': 'bold'}
-    matplotlib.rc("font", **font)
+    matplotlib.font_manager.fontManager.addfont('simfang.ttf')
+
+    plt.rcParams['font.sans-serif']=['simfang'] #用来正常显示中文标签
+
+    plt.rcParams['axes.unicode_minus']=False#用来正常显示负号
     # 提取性别列
     gender_counts = df['GENDER'].value_counts()
 
@@ -76,7 +82,11 @@ def plot_gender_distribution(df):
 
 def plot_most_used_apps(df):
 
+    matplotlib.font_manager.fontManager.addfont('simfang.ttf')
 
+    plt.rcParams['font.sans-serif']=['simfang'] #用来正常显示中文标签
+
+    plt.rcParams['axes.unicode_minus']=False#用来正常显示负号
     # 提取最常使用程序的数据
     most_used_apps = df['最常使用的程序'].value_counts().head(10)
 
